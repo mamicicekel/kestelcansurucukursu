f5034a60d4ec46dda451c69655e922e8da61cf3b
mamicicekel <muhammedcicekel@gmail.com>
2023-09-10 15:33:21 +0300

    The classes page is finished.

--- src/pages/drivingLicence/classes/Classes.js
+++ src/pages/drivingLicence/classes/Classes.js
@@ -18,7 +18,69 @@ export default function Classes() {
     <div>
       <Header />
       <PageHeader hasLink prevPageName="Ehliyet" pageName="Ehliyet Sınıfları" />
-      
+      <main>
+        <Grid container spacing={2} columns={12}>
+          <Grid item xs={12} md={4}>
+            <Card sx={{ maxWidth: 400 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="250"
+                  image={Motorcycle}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    A1, A2 Motosiklet Ehliyeti
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">
+                    A1 ehliyeti motor silindir hacmi 125 cc'ye kadar, A2 ehliyeti ise motor silindir hacmi 600 cc'ye kadar olan motosikletleri kullanmak için gereklidir.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Card sx={{ maxWidth: 400 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="250"
+                  image={Car}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    B Sınıfı Otomobil Ehliyeti
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">
+                    Otomobil ve kamyonet kullanabilmek için gereken ehliyettir. Bu sınıfa ait sürücü belgeleri aynı zamanda M, B1 ve F sınıflarını da kapsıyor.
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Card sx={{ maxWidth: 400 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="250"
+                  image={Truck}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    C Kamyon Ehliyeti
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">
+                    Kamyon, tır ve çekici gibi araçları kullanabilmek için gereken C sınıfı sürücü belgesi halk arasında kamyon ehliyeti olarak da biliniyor.                </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </main>
       <Footer />
     </div>
   )
