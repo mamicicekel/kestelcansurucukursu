8d7c3af011ab6d467dfcc83b1f67e341a505b03f
mamicicekel <muhammedcicekel@gmail.com>
2023-09-11 16:00:11 +0300

    The about page is finished.

--- src/pages/company/about/About.js
+++ src/pages/company/about/About.js
@@ -1,13 +1,140 @@
 import React from 'react'
+import '../about/About.css'
 import Header from '../../../components/header/Header'
 import Footer from '../../../components/footer/Footer'
 import PageHeader from '../../../components/pageHeader/PageHeader'
+import Grid from '@mui/material/Grid';
+import CourseSign2 from '../../../assets/kurs-tabela-1.jpeg'
+import CourseSign1 from '../../../assets/kurs-tabela-3.jpeg'
+import Egitmen1 from '../../../assets/egitmen-1.png'
+import Egitmen2 from '../../../assets/egitmen-2.png'
+import Egitmen3 from '../../../assets/egitmen-3.png'
+import Egitmen4 from '../../../assets/egitmen-4.png'
+import Egitmen5 from '../../../assets/egitmen-5.png'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
 export default function About() {
 
   return (
     <div>
       <Header />
       <PageHeader hasLink prevPageName="Kurumsal" pageName="Hakkımızda" />
+      <main className='main-about'>
+        <Grid container spacing={2} columns={12} marginBottom={30}>
+          <Grid item xs={12} md={6}>
+            <section className='parent'>
+              <img src={CourseSign1} className='image1' />
+              <img src={CourseSign2} className='image2' />
+            </section>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <h1>
+            Güvenli ve Yetkin Sürücüler Yetiştirmeyi Hedefliyoruz
+            </h1>
+            <p>Sürücü Kursu olarak, sürücülük konusundaki kapsamlı deneyimimiz ve uzman eğitmen kadromuzla, güvenli ve yetkin sürücüler yetiştirmeyi hedefliyoruz. Misyonumuz, öğrencilerimize trafik kurallarını ve yol güvenliğini öğretmek, araçları etkili bir şekilde kullanmayı öğretmek ve gelecekteki sürücülerin trafikte kendilerini ve diğerlerini güvende hissetmelerini sağlamaktır. Eğitim programlarımız, katılımcıların ihtiyaçlarına ve seviyelerine uygun olarak özelleştirilmiştir. Modern eğitim araçları, simülatörler ve pratik sürüş dersleri ile öğrencilerimizin kendilerine olan güvenlerini arttırmayı ve sürücülük becerilerini geliştirmeyi amaçlıyoruz. Sürücü kursumuz, kaliteli eğitim anlayışı, disiplinli yaklaşımı ve güvenilir hizmetleriyle tanınmaktadır.</p>
+          </Grid>
+        </Grid>
+        <div className='teachers'>
+          <h3>Eğitmenlerimiz</h3>
+          <h1>Sürüş Konusunda Büyük Deneyime Sahibiz</h1>
+        </div>
+        <Grid container spacing={2} columns={10}>
+          <Grid item xs={12} md={2}>
+            <Card sx={{ maxWidth: 400, backgroundColor:'#F4F6F8' }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="250"
+                  image={Egitmen1}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                  Pınar Çiçekel
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">Eğitmen</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Card sx={{ maxWidth: 400, backgroundColor:'#F4F6F8' }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="250"
+                  image={Egitmen2}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                  Erol Yılmaz
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">Eğitmen</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Card sx={{ maxWidth: 400, backgroundColor:'#F4F6F8' }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="250"
+                  image={Egitmen3}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                  Battal Erdurucan
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">Eğitmen</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Card sx={{ maxWidth: 400, backgroundColor:'#F4F6F8' }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="250"
+                  image={Egitmen4}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                  Gülbahar
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">Eğitmen</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={2}>
+            <Card sx={{ maxWidth: 400, backgroundColor:'#F4F6F8',  }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="250"
+                  image={Egitmen5}
+                  alt="green iguana"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                  Ahmet Çiçekel
+                  </Typography>
+                  <Typography variant="body1" color="text.secondary">Eğitmen</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        </Grid>
+      </main>
       <Footer />
     </div>
   )
