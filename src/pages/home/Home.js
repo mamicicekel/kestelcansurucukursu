80791974824ad013a2ef504d2c977599e57eba1e
mamicicekel <muhammedcicekel@gmail.com>
2023-09-13 22:45:18 +0300

    The functional features of the homepage carousel have been added.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,40 +9,58 @@ import { Grid } from '@mui/material'
 import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from "react-icons/bs";
 
 export default function Home() {
-
   const carouselImages = [
     {
       title: 'HEM MANUEL HEM DE OTOMATİK VİTES',
-      url: `${Car}`,
+      url: Car,
       alt: 'b sınıfı ehliyet'
     },
     {
       title: 'TÜM CC MOTOR EHLİYET BELGESİNİ VERİYORUZ',
-      url: `${Motorcycle}`,
+      url: Motorcycle,
       alt: 'a sınıfı ehliyet'
     }
   ]
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === carouselImages.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? carouselImages.length - 1 : prevIndex - 1));
+  };
 
   return (
     <div>
       <Header />
       <main className='main-home'>
         <section className='carousel-container'>
-          <img src={Slayt} />
+          <img src={Slayt} alt="Carousel" className='carousel-background-image'/>
           <div className="carousel-content">
             <Grid container spacing={2} columns={12}>
-              <Grid item xs={2} md={6}>
+              <Grid item xs={12} md={6}>
                 <h5>ALANINDA UZMAN EĞİTMENLER İLE </h5>
-                <h1>HEM MANUEL HEM DE OTOMATİK VİTES</h1>
+                <h3>{carouselImages[currentIndex].title}</h3>
                 <p>Alanında Uzman Eğitmenler ve tecrübeli kadromuzla; yenilikçi eğitim anlayışını benimseyip öğrenci merkezli eğitim veren bir kurumdur.</p>
               </Grid>
-              <Grid item xs={12} md={6}>
-                <img src={Car} />
+              <Grid item xs={12} md={6} >
+                <img src={carouselImages[currentIndex].url} alt={carouselImages[currentIndex].alt} width='600px' />
               </Grid>
             </Grid>
           </div>
-          <button className='carousel-button prev'><BsFillArrowLeftCircleFill /></button>
-          <button className='carousel-button next'><BsFillArrowRightCircleFill /></button>
+          <div className="carousel-dots">
+            {carouselImages.map((_, index) => (
+              <span
+                key={index}
+                className={`carousel-dot ${index === currentIndex ? 'active' : ''}`}
+                onClick={() => setCurrentIndex(index)}
+              ></span>
+            ))}
+          </div>
+          <button className='carousel-button prev' onClick={prevSlide}><BsFillArrowLeftCircleFill /></button>
+          <button className='carousel-button next' onClick={nextSlide}><BsFillArrowRightCircleFill /></button>
         </section>
       </main>
       <Footer />
