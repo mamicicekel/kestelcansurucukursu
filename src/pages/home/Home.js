2b55e1f040da1baeb5ca466fa3fbb420de3b1130
mamicicekel <muhammedcicekel@gmail.com>
2023-09-14 13:52:44 +0300

    About section has been added in home page.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,7 +8,7 @@ import Motorcycle from '../../assets/motorcycle.png'
 import { Grid } from '@mui/material'
 import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from "react-icons/bs";
 import { AiFillCar } from 'react-icons/ai'
-import {GiSteeringWheel, GiCarSeat} from 'react-icons/gi'
+import { GiSteeringWheel, GiCarSeat } from 'react-icons/gi'
 import CourseSign2 from '../../assets/kurs-tabela-1.jpeg'
 import CourseSign1 from '../../assets/kurs-tabela-3.jpeg'
 import { Link } from 'react-router-dom'
@@ -71,28 +71,44 @@ export default function Home() {
           <Grid container spacing={2} columns={12}>
             <Grid item xs={12} md={4}>
               <div className='info-box'>
-                <AiFillCar size={'60px'} color='#F3BD00'/>
+                <AiFillCar size={'60px'} color='#F3BD00' />
                 <h2>Kolayca Araba Sürmeyi Öğrenin</h2>
                 <p>Pratik odaklı yaklaşımımızla kolayca araba sürmeyi öğrenin ve yola emin adımlarla çıkın.</p>
               </div>
             </Grid>
             <Grid item xs={12} md={4}>
               <div className='info-box'>
-                <GiSteeringWheel size={'60px'} color='#F3BD00'/>
+                <GiSteeringWheel size={'60px'} color='#F3BD00' />
                 <h2>Direksiyon Eğitimi</h2>
                 <p>Alanında uzman Bay-Bayan kişilerden oluşan deneyimli kadromuz ile direksiyon dersleri vermekteyiz.</p>
               </div>
             </Grid>
             <Grid item xs={12} md={4}>
               <div className='info-box'>
-                <GiCarSeat size={'60px'} color='#F3BD00'/>
+                <GiCarSeat size={'60px'} color='#F3BD00' />
                 <h2>Simülatör</h2>
                 <p>Teknolojiyi her zaman yakından takip eden kurumumuz, simülatör ile sizlere eşsiz bir eğitim vermektedir.</p>
               </div>
             </Grid>
           </Grid>
         </section>
-       
+        <section className='about-section'>
+          <Grid container spacing={2} columns={12} marginBottom={30}>
+            <Grid item xs={12} md={6}>
+              <section className='parent'>
+                <img src={CourseSign1} className='image1' />
+                <img src={CourseSign2} className='image2' />
+              </section>
+            </Grid>
+            <Grid item xs={12} md={6} sx={{ display: 'flex', flexDirection: 'column' }}>
+              <h1>
+                Güvenli ve Yetkin Sürücüler Yetiştirmeyi Hedefliyoruz
+              </h1>
+              <p>Sürücü Kursu olarak, sürücülük konusundaki kapsamlı deneyimimiz ve uzman eğitmen kadromuzla, güvenli ve yetkin sürücüler yetiştirmeyi hedefliyoruz. Misyonumuz, öğrencilerimize trafik kurallarını ve yol güvenliğini öğretmek, araçları etkili bir şekilde kullanmayı öğretmek ve gelecekteki sürücülerin trafikte kendilerini ve diğerlerini güvende hissetmelerini sağlamaktır. Eğitim programlarımız, katılımcıların ihtiyaçlarına ve seviyelerine uygun olarak özelleştirilmiştir. Modern eğitim araçları, simülatörler ve pratik sürüş dersleri ile öğrencilerimizin kendilerine olan güvenlerini arttırmayı ve sürücülük becerilerini geliştirmeyi amaçlıyoruz. Sürücü kursumuz, kaliteli eğitim anlayışı, disiplinli yaklaşımı ve güvenilir hizmetleriyle tanınmaktadır.</p>
+              <Link to="/hakkimizda" className="button">Daha fazlasını oku</Link>
+            </Grid>
+          </Grid>
+        </section>
       </main>
       <Footer />
     </div>
